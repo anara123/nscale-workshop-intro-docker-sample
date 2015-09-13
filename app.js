159db9bf72e52d61dd9ad527dfd71ec34e3e415e
Anar Azadaliyev <anar.azadaliyev@gmail.com>
2015-09-13 15:31:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello Anar !!\n');
 }).listen(port);
 console.log('Server running on port', port);
